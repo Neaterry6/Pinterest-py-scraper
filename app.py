1792987818400c8081624fd1194c0d38f0c885ac
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2026-04-21 22:58:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,38 @@
-
-
 import requests
 from bs4 import BeautifulSoup
 import random
-import time
+import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# Static list of user agents
 user_agents = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0',
-    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.3',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
 ]
 
 def get_user_agent():
     return random.choice(user_agents)
 
-def scrape_pinterest(url):
-    user_agent = get_user_agent()
-    headers = {'User-Agent': user_agent}
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    images = soup.find_all('img')
-    image_urls = [img.get('src') for img in images if img.get('src')]
-    return image_urls
+def scrape_pinterest(keyword, num):
+    try:
+        headers = {'User-Agent': get_user_agent()}
+        url = f"https://www.pinterest.com/search/pins/?q={keyword}"
+        response = requests.get(url, headers=headers, timeout=10)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'html.parser')
+        images = soup.find_all('img')
+        image_urls = [img.get('src') for img in images if img.get('src')]
+        return image_urls[:num]
+    except Exception as e:
+        return [f"Error: {str(e)}"]
 
-@app.route('/scrape', methods=['GET'])
-def main():
-    url = 'https://www.pinterest.com/'
-    image_urls = scrape_pinterest(url)
-    return jsonify({'image_urls': image_urls})
+@app.route('/pin/<keyword>/<int:num>', methods=['GET'])
+def pin(keyword, num):
+    image_urls = scrape_pinterest(keyword, num)
+    return jsonify({'keyword': keyword, 'count': num, 'image_urls': image_urls})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
